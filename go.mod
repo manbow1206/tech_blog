8f587aa56d3d651542625cd29a07b5e701f8bc90
manbow1206 <tyasunaga@iyell.jp>
2022-04-16 15:41:34 +0900

    add : html rendering

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module blog
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.7.2
+require (
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3
+	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/labstack/echo/v4 v4.7.2
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+const tmplPath = "src/template/"
 var e = createMux()
 
 func main() {
@@ -24,10 +27,32 @@ func createMux() *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
 
+	e.Static("/css", "src/css")
 	// アプリケーションインスタンスを返却
 	return e
 }
 
 func articleIndex(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	data := map[string]interface{}{
+		"message": "Hello World",
+		"Now" : time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	// 定義したhtmlBlob()関数を呼び出し、生成されたHTMLをバイトデータとして受け取る
+	b, err := htmlBlob(file, data)
+
+	// エラーチェック
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// ステータスコード200でHTMLデータをレスポンス
+	return c.HTMLBlob(http.StatusOK, b)
 }
